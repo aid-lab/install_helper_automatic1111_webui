c601b2d42cfde075659c2bff8cab997ebbec566c
Felix Sewing <121240566+fsewing@users.noreply.github.com>
2023-01-11 16:50:09 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,14 +11,14 @@ from array import *
 
 link_array = []
 
-link_array.insert(1, ["stabilityai/stable-diffusion-2-1", "v2-1_768-nonema-pruned.ckpt"])
-link_array.insert(2, ["runwayml/stable-diffusion-v1-5", "v1-5-pruned-emaonly.ckpt"])
-link_array.insert(3, ["runwayml/stable-diffusion-v1-5", "v1-5-pruned.ckpt"])
-link_array.insert(4, ["stabilityai/stable-diffusion-2-base", "512-base-ema.ckpt"])
-link_array.insert(5, ["runwayml/stable-diffusion-inpainting", "sd-v1-5-inpainting.ckpt"])
-link_array.insert(6, ["stabilityai/stable-diffusion-x4-upscaler", "x4-upscaler-ema.ckpt"])
-link_array.insert(7, ["Fictiverse/Stable_Diffusion_PaperCut_Model", "PaperCut_v1.ckpt"])
-link_array.insert(8, ["Fictiverse/Stable_Diffusion_Microscopic_model", "Microscopic_v1.ckpt"])
+link_array.insert(0, ["stabilityai/stable-diffusion-2-1", "v2-1_768-nonema-pruned.ckpt"])
+link_array.insert(1, ["runwayml/stable-diffusion-v1-5", "v1-5-pruned-emaonly.ckpt"])
+link_array.insert(2, ["runwayml/stable-diffusion-v1-5", "v1-5-pruned.ckpt"])
+link_array.insert(3, ["stabilityai/stable-diffusion-2-base", "512-base-ema.ckpt"])
+link_array.insert(4, ["runwayml/stable-diffusion-inpainting", "sd-v1-5-inpainting.ckpt"])
+link_array.insert(5, ["stabilityai/stable-diffusion-x4-upscaler", "x4-upscaler-ema.ckpt"])
+link_array.insert(6, ["Fictiverse/Stable_Diffusion_PaperCut_Model", "PaperCut_v1.ckpt"])
+link_array.insert(7, ["Fictiverse/Stable_Diffusion_Microscopic_model", "Microscopic_v1.ckpt"])
 
 
 DIR = "models"
@@ -38,7 +38,9 @@ for i in link_array:
     os.system('cp '+ return_val +' ./stable-diffusion-webui/models/Stable-diffusion/'+i[1])  
     print("Modell "+ i[1] +" wird in den Ordner /stable-diffusion-webui/models/Stable-diffusion/ kopiert.")
     print(" ")
- 
+
+os.system('cp ./stable-diffusion-webui/v2-inference-v.yaml ./stable-diffusion-webui/models/Stable-diffusion/v2-1_768-nonema-pruned.yaml')
+
 EOF
 )
 
